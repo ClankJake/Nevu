815eca46aad69099c8e7209b2b078783657e19ed
ClankJake <44306273+ClankJake@users.noreply.github.com>
2025-08-06 15:44:12 -0300

    Update App.tsx
    Melhor suporte para mobile

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,132 +1,144 @@
-import React, { useEffect } from "react";
-import AppBar from "./components/AppBar";
-import { Route, Routes, useLocation, useNavigate } from "react-router-dom";
+import {
+  AppBar as MuiAppBar,
+  AppBarProps,
+  Box,
+  Button,
+  Drawer,
+  IconButton,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemText,
+  Toolbar,
+  Typography,
+  useMediaQuery,
+  useTheme,
+} from "@mui/material";
+import { Link, useLocation } from "react-router-dom";
+import { useUserSession } from "../states/UserSession";
+import { useMemo, useState } from "react";
+import { Menu as MenuIcon } from "@mui/icons-material";
 
-import Browse from "./pages/Browse";
-import { Box } from "@mui/material";
-import Watch from "./pages/Watch";
-import Login from "./pages/Login";
-import Search from "./pages/Search";
-import Home from "./pages/Home";
-import Library from "./pages/Library";
-import BigReader from "./components/BigReader";
-import { useWatchListCache } from "./states/WatchListCache";
-import Startup, { useStartupState } from "./pages/Startup";
-import PerPlexedSync from "./components/PerPlexedSync";
-import WaitingRoom from "./pages/WaitingRoom";
-import ToastManager from "./components/ToastManager";
-import LibraryScreen from "./components/LibraryScreen";
-import { useSessionStore } from "./states/SessionState";
-import Settings from "./pages/Settings";
-import { useUserSettings } from "./states/UserSettingsState";
-import MetaScreen from "./components/MetaScreen";
-import ConfirmModal from "./components/ConfirmModal";
+// Componente para o menu lateral em dispositivos móveis
+const MobileDrawer = ({
+  pages,
+  onClose,
+  open,
+}: {
+  pages: { name: string; path: string }[];
+  onClose: () => void;
+  open: boolean;
+}) => (
+  <Drawer anchor="left" open={open} onClose={onClose}>
+    <Box
+      sx={{ width: 250 }}
+      role="presentation"
+      onClick={onClose}
+      onKeyDown={onClose}
+    >
+      <List>
+        {pages.map((page) => (
+          <ListItem key={page.name} disablePadding>
+            <ListItemButton component={Link} to={page.path}>
+              <ListItemText primary={page.name} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  </Drawer>
+);
 
-function AppManager() {
-  const { loading } = useStartupState();
-  const [showApp, setShowApp] = React.useState(false);
-  const [fadeOut, setFadeOut] = React.useState(false);
-
-  useEffect(() => {
-    if (loading) return;
-
-    setTimeout(() => {
-      setFadeOut(true);
-      setTimeout(() => setShowApp(true), 500);
-    }, 1000);
-  }, [loading]);
-
-  if (!showApp) {
-    return (
-      <div style={{ opacity: fadeOut ? 0 : 1, transition: "opacity 0.5s" }}>
-        <Startup />
-      </div>
-    );
-  }
-
-  return <App />;
-}
-
-function AppTitleManager() {
-  const { PlexServer } = useSessionStore();
-
-  useEffect(() => {
-    console.log(PlexServer);
-    if (!PlexServer?.friendlyName) return;
-
-    const capitalizedFriendlyName =
-      PlexServer.friendlyName.charAt(0).toUpperCase() +
-      PlexServer.friendlyName.slice(1);
-    document.title = `${capitalizedFriendlyName} - Nevu`;
-  }, [PlexServer]);
-
-  useEffect(() => {
-    document.title = "Nevu";
-  }, []);
-
-  return <></>;
-}
-
-function App() {
+const AppBar = (props: AppBarProps) => {
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down("md"));
+  const { user, server } = useUserSession();
   const location = useLocation();
-  const navigate = useNavigate();
 
-  useEffect(() => {
-    if (
-      !localStorage.getItem("accessToken") &&
-      !location.pathname.startsWith("/login")
-    )
-      navigate("/login");
-  }, [location.pathname, navigate]);
+  const [drawerOpen, setDrawerOpen] = useState(false);
 
-  useEffect(() => {
-    useWatchListCache.getState().loadWatchListCache();
-    useSessionStore.getState().fetchPlexServer();
-    useUserSettings.getState().fetchSettings();
+  const pages = useMemo(
+    () => [
+      { name: "Home", path: "/" },
+      { name: "Browse", path: "/browse" },
+      { name: "Library", path: "/library" },
+      { name: "Search", path: "/search" },
+      { name: "Settings", path: "/settings" },
+    ],
+    []
+  );
 
-    const interval = setInterval(() => {
-      useWatchListCache.getState().loadWatchListCache();
-    }, 60000);
-
-    return () => clearInterval(interval);
-  }, []);
+  const handleDrawerToggle = () => {
+    setDrawerOpen(!drawerOpen);
+  };
 
   return (
-    <>
-      <BigReader />
-      <PerPlexedSync />
-      <ToastManager />
-      <LibraryScreen />
-      <AppTitleManager />
-      <MetaScreen />
-      <ConfirmModal />
-      <Routes>
-        <Route path="*" element={<AppBar />} />
-        <Route path="/watch/:itemID" element={<></>} />
-        <Route path="/sync/waitingroom" element={<></>} />
-      </Routes>
-      <Box
-        sx={{
-          width: "100%",
-          height: "auto",
-        }}
-      >
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/browse/:libraryID" element={<Browse />} />
-          <Route path="/watch/:itemID" element={<Watch />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/search/:query?" element={<Search />} />
-          <Route path="/sync/waitingroom" element={<WaitingRoom />} />
-          <Route
-            path="/library/:libraryKey/dir/:dir/:subdir?"
-            element={<Library />}
-          />
-          <Route path="/settings/*" element={<Settings />} />
-        </Routes>
-      </Box>
-    </>
+    <MuiAppBar {...props}>
+      <Toolbar>
+        {isMobile ? (
+          <>
+            <IconButton
+              color="inherit"
+              aria-label="open drawer"
+              edge="start"
+              onClick={handleDrawerToggle}
+              sx={{ mr: 2 }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <MobileDrawer
+              pages={pages}
+              open={drawerOpen}
+              onClose={handleDrawerToggle}
+            />
+          </>
+        ) : null}
+        <Typography
+          variant="h6"
+          noWrap
+          component={Link}
+          to="/"
+          sx={{
+            mr: 2,
+            flexGrow: isMobile ? 1 : 0,
+            fontFamily: "monospace",
+            fontWeight: 700,
+            letterSpacing: ".3rem",
+            color: "inherit",
+            textDecoration: "none",
+          }}
+        >
+          Nevu
+        </Typography>
+        {!isMobile && (
+          <Box sx={{ flexGrow: 1, display: "flex" }}>
+            {pages.map((page) => (
+              <Button
+                key={page.name}
+                component={Link}
+                to={page.path}
+                sx={{
+                  my: 2,
+                  color: "white",
+                  display: "block",
+                  ...(location.pathname === page.path && {
+                    backgroundColor: "rgba(255, 255, 255, 0.1)",
+                  }),
+                }}
+              >
+                {page.name}
+              </Button>
+            ))}
+          </Box>
+        )}
+        <Box sx={{ flexGrow: 0 }}>
+          <Typography>
+            {user?.title}@{server?.name}
+          </Typography>
+        </Box>
+      </Toolbar>
+    </MuiAppBar>
   );
-}
+};
 
-export default AppManager;
+export default AppBar;
